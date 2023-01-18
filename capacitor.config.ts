641884fcb12e00c7dafd54d9ab5b9b7c2b7e886a
m2gi-bonatov <Vincent.Bonato@etu.univ-grenoble-alpes.fr>
2023-01-18 14:31:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'COChavanod',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
